1f77e9b724bc967c5234b7186e3a4383be6d68e4
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2023-01-13 10:02:01 +0100

    fix env file

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -2,8 +2,11 @@ package config
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -11,7 +14,18 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:XXXX@tcp(127.0.0.1:3306)/go_rest?charset=utf8&parseTime=True")
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Println(err)
+	}
+
+	dbName:= os.Getenv("DB_NAME")
+	
+	dbPassword:= os.Getenv("DB_PASSWORD")
+	
+	dbUser:= os.Getenv("DB_USER")
+
+	d, err := gorm.Open("mysql", dbUser+":"+dbPassword+"@tcp(127.0.0.1:3306)/"+dbName+"?charset=utf8&parseTime=True")
 	if err != nil {
 		fmt.Println("connection failed to open")
 	}
